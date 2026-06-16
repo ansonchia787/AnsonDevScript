5f707165fa48e7672c4abe7504dffa9dc3956108
ansonchia787 <yingqianxie73@gmail.com>
2026-06-16 18:55:22 +0800

    Create gamelist.lua

---
+++ gamelist.lua
@@ -0,0 +1,6 @@
+local Games = {
+    [128784467030899] = "https://raw.githubusercontent.com/ansonchia787/AnsonDevScript/main/mergeanuke.lua", -- Merge A Nuke
+    [99713568470959]  = "https://raw.githubusercontent.com/ansonchia787/AnsonDevScript/main/mopeddrag.lua" -- Moped Drag
+}
+
+return Games
