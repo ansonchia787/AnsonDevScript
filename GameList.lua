de22770ae17091ae2c09877abbdb68ce28df31d5
ansonchia787 <yingqianxie73@gmail.com>
2026-06-17 16:49:20 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
   [128784467030899] = "https://raw.githubusercontent.com/ansonchia787/AnsonDev/main/Merge%20A%20Nuke.lua", --Merge A Nuke
+  [99713568470959] = "https://raw.githubusercontent.com/ansonchia787/AnsonDev/main/Moped%20Drag.lua", --Moped Drag
 }
 
 return Games
